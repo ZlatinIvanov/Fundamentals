e164bf72fe6490a0232175afd6c357612e41d80d
ZlatinIvanov <zlatin89@gmail.com>
2023-04-18 07:58:50 +0300

    Update _6. Odd Occurrences.py

--- 25. Dictionaries/_6. Odd Occurrences.py
+++ 25. Dictionaries/_6. Odd Occurrences.py
@@ -1,11 +1,2 @@
-words = input().lower()
-words = words.split()
-dict = {}
-for x in words:
-    if x in dict:
-        dict[x] += 1
-    else:
-        dict[x] = 1
-for word, value in dict.items():
-    if value % 2 != 0:
-        print(word, end=" ")
+
+ 
